19803cf4912b7acf04797e9c354ce805105b6600
Dacen12 <alkisalki1@hotmail.com>
2022-03-07 15:22:34 +0100

    trying without cors

--- main.js
+++ main.js
@@ -5,13 +5,9 @@ const app = express()
 const db = require('./db')
 const pdf=  require('./pdf/pdf')
 
-const corsOptions = {
-    origin : 'https://shopee-frontend.herokuapp.com/',
-    optionsSuccessStatus: 200
-}
 
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
