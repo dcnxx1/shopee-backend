fb5ce1be7a2aacd50eebda0b52f7561acb3c2f6e
Dacen12 <alkisalki1@hotmail.com>
2022-03-07 23:44:10 +0100

    nxt

--- main.js
+++ main.js
@@ -12,10 +12,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use('/', (req,res) => {
+app.use('/', (req,res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next()
 })
 
 app.get('/', async (req,res) => {
