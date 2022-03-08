8bfbe6cde06bde6ad5787fc76096ec4951a3817e
Dacen12 <alkisalki1@hotmail.com>
2022-03-08 11:44:34 +0100

    adding cors

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ app.use((req,res, next) => {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
