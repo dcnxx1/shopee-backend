9096d4702945dfcdcdc5c3319d2ba823768138f5
Dacen12 <alkisalki1@hotmail.com>
2022-03-08 11:16:11 +0100

    adding headers

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ app.get('/dames', async (req,res) => {
 app.use('/pdf' , async (req,res, next) => {
     const generatePdf = await pdf.make(req.body.arrayBag) 
     req.pdf = generatePdf 
-    res.header('Access-Control-Allow-Origin', 'https://shopee-frontend.herokuapp.com')
+    res.set('Access-Control-Allow-Origin', 'https://shopee-frontend.herokuapp.com')
     next()
 })
 
