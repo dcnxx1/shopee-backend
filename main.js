ef2d8f0ba184f8bddc56c8f1efd33049f0d98de9
Dacen12 <alkisalki1@hotmail.com>
2022-03-08 11:56:23 +0100

    origin

--- main.js
+++ main.js
@@ -6,13 +6,11 @@ const db = require('./db')
 const pdf=  require('./pdf/pdf')
 
 
+const options ={
+    origin :'https://shopee-frontend.herokuapp.com'
+}
 
-app.use((req,res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next()
-})
-app.use(cors())
+app.use(cors(options))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
