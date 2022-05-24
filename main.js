0bcb1cc578fbfbb4dd700cdcaabebb169615e949
Dacen12 <alkisalki1@hotmail.com>
2022-05-24 16:31:29 +0200

    pushing

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const db = require('./db')
 const pdf=  require('./pdf/pdf')
 
 
+
 const options ={
     origin :'*'
 }
@@ -18,6 +19,7 @@ app.use(express.urlencoded({extended: true}))
 
 app.get('/', async (req,res) => {
 const response = await db.getAll()
+
 res.send(response.rows)
 })
 
